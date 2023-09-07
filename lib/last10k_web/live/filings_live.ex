83372c148ee902b078c2b0c754a812db458ca861
Amar Kota <amar@amarkota.com>
2023-09-06 23:04:46 -0700

    periodically update LiveView with timer

--- lib/last10k_web/live/filings_live.ex
+++ lib/last10k_web/live/filings_live.ex
@@ -3,12 +3,21 @@ defmodule Last10kWeb.FilingsLive do
 
   def render(assigns) do
     ~H"""
-    Render Filings...
+    Render Filings...<%= @myvalue %>
     """
   end
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    if connected?(socket), do: Process.send_after(self(), :update, 1000)
+
+    myvalue = 0
+    {:ok, assign(socket, myvalue: myvalue)}
+  end
+
+  def handle_info(:update, socket) do
+    Process.send_after(self(), :update, 1000)
+    myvalue = :rand.uniform(100)
+    {:noreply, assign(socket, :myvalue, myvalue)}
   end
 
 end

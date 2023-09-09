1829983c6b2819ea02ecd69c2275ebc44d3f37cf
Amar Kota <amar@amarkota.com>
2023-09-08 18:42:15 -0700

    added httpoison and fast_rss from hex

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule Last10k.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:httpoison, "~> 2.1.0"},
+      {:fast_rss, "~> 0.5.0"}
     ]
   end
 
